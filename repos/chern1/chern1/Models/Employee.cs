0e8dd2588567d33e7b5115966ec3d611741ec815
Sofiia Nikitina <71656291+ma-sonya@users.noreply.github.com>
2022-07-16 20:02:51 +0300

    Delete repos/chern1 directory

--- repos/chern1/chern1/Models/Employee.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace chern1.Models
-{
-    public class Employee
-    {
-        public int Id { get; set; } 
-        public string Name { get; set; }    
-        public DateTime WorkFrom { get; set; }
-    }
-}
--- repos/chern1/chern1/Models/Position.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace chern1.Models
-{
-    public class Position
-    {
-        public int Id { get; set; }
-        public string PostName { get; set; }
-    }
-}
